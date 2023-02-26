08d1c80152c92a291d9d9fd40cd521b758e947e7
AKSki008 <agnieszka.kwiecienskiba@gmail.com>
2023-02-26 16:25:20 +0100

    EDIT: licznik

--- JS/licznik.js
+++ JS/licznik.js
@@ -1,2 +1,13 @@
-const LICZNIK = document.getElementById(".licznik");
-console.log(LICZNIK);
+let licznik = document.querySelector(".licznik");
+let liczPages = document.querySelector(".howmuch")
+let stron = document.querySelectorAll(".boxes .pages")
+let aplikacji = document.querySelectorAll(".boxes .apps")
+
+let year = new Date();
+let month = new Date();
+let days = new Date();
+
+// licznik.innerHTML = year.getFullYear()-2023 + " lat, " ;
+licznik.innerHTML =  month.getMonth()-1 + " miesiąc i " + days.getDate() + " dzień";
+liczPages.innerHTML =  stron.length+1 + " stron i " +aplikacji.length + " aplikacji";
+

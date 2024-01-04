b56f31ab78c5e6115840c6ba015d250987516947
A.K-Ski <agnieszka.kwiecienskiba@gmail.com>
2024-01-04 13:10:46 +0100

    EDIT

--- JS/licznik.js
+++ JS/licznik.js
@@ -6,8 +6,23 @@ let aplikacji = document.querySelectorAll(".boxes .apps")
 let year = new Date();
 let month = new Date();
 let days = new Date();
+let a = year.getFullYear()-2023;
+let b = month.getMonth()-1;
+let today = new Date();
 
-// licznik.innerHTML = year.getFullYear()-2023 + " lat, " ;
-licznik.innerHTML =  month.getMonth()-1 + " miesięcy i " + days.getDate() + " dni";
+if( b==-1){
+    licznik.innerHTML =  a-1 + " rok, " + (b=11) + " miesięcy i " + days.getDate() + " dni";
+}else if(b==0){
+    licznik.innerHTML =  a + " rok, " + (b=0) + " miesięcy i " + days.getDate() + " dni";
+}
+else if(b==1){
+    licznik.innerHTML =  a + " rok, " + (b=2) + " miesięcy i " + days.getDate() + " dni";
+}
+else{
+    licznik.innerHTML =  a + " rok, " + (b=b+1) + " miesięcy i " + days.getDate() + " dni";
+};
+
+
+// licznik.innerHTML =  a + " rok i " + b + " miesięcy i " + days.getDate() + " dni";
 liczPages.innerHTML =  stron.length+1 + " stron i " +aplikacji.length + " aplikacji";
 
